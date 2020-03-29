0bc8729e744a087eca9abb6c98545d466d2f191c
Erick Chacon <eral.th07@gmail.com>
2020-03-29 15:30:55 +0100

    script to merge patches

--- patch.sh
+++ patch.sh
@@ -3,4 +3,7 @@
 git checkout master
 git branch -D patched
 git checkout -b patched
+git merge centeredmaster -m "centered master patch"
+git merge autostart -m "autostart patch"
+git merge config -m "custom config"
 
