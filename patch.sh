dcece18410aecc254fabb51c475d71bf26430e88
Erick Chacon <eral.th07@gmail.com>
2020-04-02 23:50:44 +0100

    gap

--- patch.sh
+++ patch.sh
@@ -4,6 +4,6 @@ git checkout master
 git branch -D patched
 git checkout -b patched
 git merge centeredmaster -m "centered master patch"
-git merge autostart -m "autostart patch"
 git merge config -m "custom config"
+git merge autostart -m "autostart patch"
 
