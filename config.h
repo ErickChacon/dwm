f8e29915beab1b340362d1149ffdd4158010e614
Erick Chacon <eral.th07@gmail.com>
2020-04-03 18:26:16 +0100

    wider gap

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 40;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
