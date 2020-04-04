77457c3a49fbab8b87213eeccc1fbce2f9dcca05
Erick Chacon <eral.th07@gmail.com>
2020-04-04 20:20:30 +0100

    zoom key indentation

--- config.def.h
+++ config.def.h
@@ -73,7 +73,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_r,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_z, zoom,           {0} },
+	{ MODKEY,                       XK_z,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
