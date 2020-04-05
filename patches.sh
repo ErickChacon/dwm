d26f21e54b2504239f1d279548647f3763fc55d4
Erick Chacon <eral.th07@gmail.com>
2020-04-05 23:25:19 +0100

    pathching

--- patches.sh
+++ patches.sh
@@ -6,5 +6,5 @@ git checkout -b patched
 git merge autostart -m "autostart patch"
 git merge attachbelow -m "attachbelow"
 git merge vanitygaps -m "vanitygaps"
-git merge fakefullscreen -m "fakefullscreen"
+git merge fakefullscreentoggle -m "fakefullscreen"
 git merge -Xtheirs config -m "custom config"
