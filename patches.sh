e5cc2151c04cf25629488bf8bd787f940e561e65
Erick Chacon <eral.th07@gmail.com>
2020-04-12 21:35:04 +0100

    vanitygaps with custom centered master

--- patches.sh
+++ patches.sh
@@ -5,7 +5,7 @@ git branch -D patched
 git checkout -b patched
 git merge autostart -m "autostart patch"
 git merge attachbelow -m "attachbelow"
-git merge vanitygaps -m "vanitygaps"
+git merge vanitygapss -m "vanitygaps: custom centered master"
 git merge fakefullscreentoggle -m "fakefullscreen"
-git merge movestack -m "movestack patch"
+git merge movestack -m "movestach patch"
 git merge -Xtheirs config -m "custom config"
