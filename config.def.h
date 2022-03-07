e629aca7fdac462946181620853aafac9dae6494
Erick Chacon <eral.th07@gmail.com>
2022-03-06 23:48:32 -0500

    go back to gray

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -11,7 +11,7 @@ static const char *fonts[]          = {
 };
 static const char dmenufont[]       = "DroidSansMono Nerd Font:size=11";
 static const char col_gray1[]       = "#3c3836"; /* #222222 */
-static const char col_gray2[]       = "#222222"; /* #928374 #fabd2f #444444 */
+static const char col_gray2[]       = "#928374"; /* #fabd2f #444444 */
 static const char col_gray3[]       = "#a89984"; /* #bbbbbb */
 static const char col_gray4[]       = "#a89984"; /* #3c3836 #eeeeee */
 static const char col_cyan[]        = "#504945"; /* #a89984 #8ec07c #005577 */
