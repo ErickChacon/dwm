3d00a2b895d37536a9ad201998f4768351908f65
Erick Chacon <eral.th07@gmail.com>
2022-03-06 23:04:12 -0500

    borders

--- config.def.h
+++ config.def.h
@@ -18,8 +18,8 @@ static const char col_title[]       = "#d3869b"; /* #83a598 */
 static const char col_cyan[]        = "#504945"; /* #a89984 #8ec07c #005577 */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2 },
 };
 
 /* tagging */
