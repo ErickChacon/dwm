68dcc8af7b4fbe9acaaabdc9336a77a11fb432e8
Erick Chacon <eral.th07@gmail.com>
2022-03-06 23:19:24 -0500

    title and border same color

--- config.def.h
+++ config.def.h
@@ -11,13 +11,12 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_title[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeTitle]  = { col_title, col_gray1,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
+	[SchemeTitle]  = { col_gray2, col_gray1,  col_gray1  },
 };
 
 /* tagging */
