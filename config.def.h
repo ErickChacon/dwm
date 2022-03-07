2f007ff1d3bedd4c5f2aa434dc063f02476715a6
Erick Chacon <eral.th07@gmail.com>
2022-03-06 22:32:54 -0500

    similar to tmuxline

--- config.def.h
+++ config.def.h
@@ -13,9 +13,9 @@ static const char dmenufont[]       = "DroidSansMono Nerd Font:size=11";
 static const char col_gray1[]       = "#3c3836"; /* #222222 */
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#a89984"; /* #bbbbbb */
-static const char col_gray4[]       = "#3c3836"; /* #eeeeee */
+static const char col_gray4[]       = "#a89984"; /* #3c3836 #eeeeee */
 static const char col_title[]       = "#83a598";
-static const char col_cyan[]        = "#a89984"; /* #8ec07c #005577 */
+static const char col_cyan[]        = "#504945"; /* #a89984 #8ec07c #005577 */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
