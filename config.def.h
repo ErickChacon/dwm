a4c5b14590cf5e0f2589dc40009f12149babfe04
Erick Chacon <eral.th07@gmail.com>
2022-03-06 21:23:20 -0500

    col_title

--- config.def.h
+++ config.def.h
@@ -14,6 +14,7 @@ static const char col_gray1[]       = "#504945"; /* #222222 */
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#a89984"; /* #bbbbbb */
 static const char col_gray4[]       = "#eeeeee";
+static const char col_title[]       = "#83a598";
 static const char col_cyan[]        = "#8ec07c"; /* #005577 */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
